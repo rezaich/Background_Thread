46c05a2482873d133d8555f7f64b1962d68d1ba2
rezaich <ichsanireza@gmail.com>
2021-08-20 15:42:35 +0700

    With Coroutine (kotlin)

--- app/src/main/java/com/zaich/backgroundthread/MainActivity.kt
+++ app/src/main/java/com/zaich/backgroundthread/MainActivity.kt
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import com.zaich.backgroundthread.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -15,30 +20,55 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
+/*        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())*/
 
-            binding.btnStart.setOnClickListener {
-                executor.execute {
-                    try {
-                        //simulate processing compressing
-                        for(i in 0..10){
-                            Thread.sleep(500)
-                            val percentage = i * 10
+        binding.btnStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default) {
+                //simulate process in background thread
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
 
-                            handler.post {
-                                //update ui  in main thread
-                                if (percentage  == 100) {
-                                    binding.tvStatus.setText(R.string.task_completed)
-                                }else{
-                                    binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                                }
-                            }
+                    withContext(Dispatchers.Main) {
+                        //update ui  in main thread
+                        if (percentage == 100) {
+                            binding.tvStatus.setText(R.string.task_completed)
+                        } else {
+                            binding.tvStatus.text =
+                                String.format(getString(R.string.compressing), percentage)
                         }
-                    } catch ( e: InterruptedException){
-                        e.printStackTrace()
                     }
                 }
             }
+        }
     }
+
+
+    /** Executor+Handler
+     *  binding.btnStart.setOnClickListener {
+    executor.execute {
+    try {
+    //simulate processing compressing
+    for(i in 0..10){
+    Thread.sleep(500)
+    val percentage = i * 10
+
+    handler.post {
+    //update ui  in main thread
+    if (percentage  == 100) {
+    binding.tvStatus.setText(R.string.task_completed)
+    }else{
+    binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
+    }
+    }
+    }
+    } catch ( e: InterruptedException){
+    e.printStackTrace()
+    }
+    }
+    }
+    }
+    }*/
+
 }
