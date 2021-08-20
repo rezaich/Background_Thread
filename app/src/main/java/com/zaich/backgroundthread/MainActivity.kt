a52a27c139debfd87badbd39455b734fbea2e82e
rezaich <ichsanireza@gmail.com>
2021-08-20 15:27:29 +0700

    first commit

---
+++ app/src/main/java/com/zaich/backgroundthread/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.zaich.backgroundthread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.zaich.backgroundthread.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+            binding.btnStart.setOnClickListener {
+                executor.execute {
+                    try {
+                        //simulate processing compressing
+                        for(i in 0..10){
+                            Thread.sleep(500)
+                            val percentage = i * 10
+
+                            handler.post {
+                                //update ui  in main thread
+                                if (percentage  == 100) {
+                                    binding.tvStatus.setText(R.string.task_completed)
+                                }else{
+                                    binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                                }
+                            }
+                        }
+                    } catch ( e: InterruptedException){
+                        e.printStackTrace()
+                    }
+                }
+            }
+    }
+}
